20f8fa2dca2b10250064bb9e2eb8b8471b85ed9a
DIEGO CUBILLOS <dcubillos@awake.travel>
2024-08-23 10:52:54 -0500

    firts

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import {
+  addKeyword,
+  createBot,
+  createFlow,
+  createProvider,
+  MemoryDB,
+} from "@bot-whatsapp/bot";
+
+import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys";
+
+const flowBienvenida = addKeyword("bienvenida").addAnswer(
+  "Hola, bienvenido al chat de whatsapp"
+);
+const main = async () => {
+  const provider = createProvider(BaileysProvider);
+  provider.initHttpServer(3002);
+  provider.http?.server.post(
+    "/send-message",
+    handleCtx(async (bot, req, res) => {
+      const phone = req.phone;
+      const message = req.message;
+      const mediaURl = req.mediaUrl;
+      // console.log(body)
+      await bot.sendMessage(phone, message, {
+        media: mediaURl
+      });
+      res.end("Esto es del server polka");
+    })
+  );
+  await createBot({
+    database: new MemoryDB(),
+    flow: createFlow([flowBienvenida]),
+    provider,
+  });
+};
+main();
